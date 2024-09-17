5dab1b01aac4401404532a0d31cc12854d9b2974
Akash kumar <akaysharma894@gmail.com>
2024-09-18 00:36:59 +0530

    Added changes in the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,36 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]); // These are public routes
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth().protect();
   }
 
+  //auth().userId check that its logged in
+  //auth().orgId checks that user has created organization or not.
+
   if (!auth().userId && !isPublicRoute) {
+    //If not logged in and user tyring to access any url apart from public it should send it to signIn
     return auth().redirectToSignIn({ returnBackUrl: request.url });
   }
 
   if (
     auth().userId &&
-    !auth().orgId &&
-    request.nextUrl.pathname !== "/select-org"
+    !auth().orgId && //This line checks if the user is logged in but there is no organization created.
+    request.nextUrl.pathname !== "/select-org" // and it does not belong to select-org pathname
   ) {
-    const orgSelection = new URL("/select-org", request.url);
+    const orgSelection = new URL("/select-org", request.url); // so it should redirect to it.
     return NextResponse.redirect(orgSelection);
   }
 
   if (auth().userId && isPublicRoute(request)) {
-    // Add custom logic to run before redirecting
-    let path = "/select-org";
+    //User is logged in but still on public route
+    let path = "/select-org"; // redirect user to this path
 
     if (auth().orgId) {
+      // and if loggedIn and the orgranization already exists then redirect it to is orgId.
       path = `/organization/${auth().orgId}`;
     }
     const orgSelection = new URL(path, request.url);
