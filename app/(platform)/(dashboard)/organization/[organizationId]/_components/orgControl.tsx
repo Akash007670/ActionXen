96daa1dd29497443f8d3a6ec400e5e5a6081e507
Akash kumar <akaysharma894@gmail.com>
2024-09-22 01:08:48 +0530

    Added changes for dynamic route for org list

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/orgControl.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/orgControl.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+"use client";
+
+import { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { useOrganizationList } from "@clerk/nextjs";
 
 const OrganizationControl = () => {
+  const params = useParams(); // will get the organization from the url and if someone try to change it we'll change the org also.
+
+  const { setActive } = useOrganizationList();
+
+  useEffect(() => {
+    if (!setActive) return;
+    setActive({ organization: params.organizationId as string }); // making the params.organizationId as string.
+  }, [setActive, params.organizationId]); //Also the organizationId should be spelled correctly and match with folder name.
+
   return <div>OrganizationControl</div>;
 };
 
