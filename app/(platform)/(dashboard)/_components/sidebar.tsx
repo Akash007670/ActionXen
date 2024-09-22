9ba46a526ff22a26061296e0e50f864eaef2b4f3
Akash kumar <akaysharma894@gmail.com>
2024-09-23 02:01:43 +0530

    Created sidebar component for showing all org list

---
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useLocalStorage } from "usehooks-ts"; //localstorage hook
+import { Plus } from "lucide-react";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+//Importing components.
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Accordion } from "@/components/ui/accordion";
+import { Skeleton } from "@/components/ui/skeleton";
+import NavItem, { Organization } from "./navItem";
+
+interface ISidebarProps {
+  storageKey?: string; // This storageKey means that we'll keep track of sidebar opening/closing from the localstorage
+}
+
+const Sidebar = (props: ISidebarProps) => {
+  const { storageKey = "t-sidebar-state" } = props;
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+  //Here down below you can use alias because both the function provides isLoaded method so we named something meaningfull to both of them
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true, //this is used to paginate
+    },
+  });
+
+  //this will turn into array
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((prev) => ({
+      ...prev,
+      [id]: expanded[id],
+    }));
+  };
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="font-medium text-xs flex items-center mb-1">
+        <span className="pl-4">Workspaces</span>
+        <Button
+          asChild
+          type="button"
+          size="icon"
+          variant="ghost"
+          className="ml-auto"
+        >
+          <Link href="/select-org">
+            <Plus className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+      {/* Accordion component */}
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue} //The reason to create defaultAccordion value is because the defaultValue accepts only string[] | undefined
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Sidebar;
--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,12 +1,12 @@
 import React, { ReactNode } from "react";
+import Sidebar from "../_components/sidebar";
 
 const OrganizationLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="pt-20 md:pt-24 px-4  max-w-6xl 2xl:max-w-screen-xl mx-auto border border-green-500">
+    <main className="pt-20 md:pt-24 px-4 mx-auto border border-blue-500">
       <div className="flex gap-x-7">
         <div className="w-64 shrink-0 hidden md:block border border-red-500">
-          {/* sidebar will render here */}
-          sidebar
+          <Sidebar />
         </div>
         {children}
       </div>
